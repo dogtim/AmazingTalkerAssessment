d6cf1c96399510c5d713b80289a5d3c4d739f5f5
TimChen <fatfattim@gmail.com>
2022-05-05 15:01:19 +0800

    To remove useless classes in the assessment

--- app/src/main/java/com/amazingtalker/assessment/BaseCardActivity.kt
+++ app/src/main/java/com/amazingtalker/assessment/BaseCardActivity.kt
@@ -36,35 +36,9 @@ abstract class BaseCardActivity : FragmentActivity() {
 
     protected lateinit var viewPager: ViewPager2
     private lateinit var cardSelector: Spinner
-    private lateinit var smoothScrollCheckBox: CheckBox
-    private lateinit var rotateCheckBox: CheckBox
-    private lateinit var translateCheckBox: CheckBox
-    private lateinit var scaleCheckBox: CheckBox
     private lateinit var gotoPage: Button
 
-    private val translateX get() = viewPager.orientation == ORIENTATION_VERTICAL &&
-            translateCheckBox.isChecked
-    private val translateY get() = viewPager.orientation == ORIENTATION_HORIZONTAL &&
-            translateCheckBox.isChecked
-
     protected open val layoutId: Int = R.layout.activity_no_tablayout
-
-    private val mAnimator = ViewPager2.PageTransformer { page, position ->
-        val absPos = Math.abs(position)
-        page.apply {
-            rotation = if (rotateCheckBox.isChecked) position * 360 else 0f
-            translationY = if (translateY) absPos * 500f else 0f
-            translationX = if (translateX) absPos * 350f else 0f
-            if (scaleCheckBox.isChecked) {
-                val scale = if (absPos > 1) 0F else 1 - absPos
-                scaleX = scale
-                scaleY = scale
-            } else {
-                scaleX = 1f
-                scaleY = 1f
-            }
-        }
-    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,25 +46,15 @@ abstract class BaseCardActivity : FragmentActivity() {
 
         viewPager = findViewById(R.id.view_pager)
         cardSelector = findViewById(R.id.card_spinner)
-        smoothScrollCheckBox = findViewById(R.id.smooth_scroll_checkbox)
-        rotateCheckBox = findViewById(R.id.rotate_checkbox)
-        translateCheckBox = findViewById(R.id.translate_checkbox)
-        scaleCheckBox = findViewById(R.id.scale_checkbox)
         gotoPage = findViewById(R.id.jump_button)
 
         cardSelector.adapter = createCardAdapter()
 
-        viewPager.setPageTransformer(mAnimator)
-
         gotoPage.setOnClickListener {
             val card = cardSelector.selectedItemPosition
-            val smoothScroll = smoothScrollCheckBox.isChecked
+            val smoothScroll = true
             viewPager.setCurrentItem(card, smoothScroll)
         }
-
-        rotateCheckBox.setOnClickListener { viewPager.requestTransform() }
-        translateCheckBox.setOnClickListener { viewPager.requestTransform() }
-        scaleCheckBox.setOnClickListener { viewPager.requestTransform() }
     }
 
     private fun createCardAdapter(): SpinnerAdapter {
