5d6d6f41883777c67fbf797a2c1345506fe40849
TimChen <fatfattim@gmail.com>
2022-05-06 13:40:09 +0800

    Give layout of timeline

--- app/src/main/java/com/amazingtalker/assessment/CoursesAdapter.kt
+++ app/src/main/java/com/amazingtalker/assessment/CoursesAdapter.kt
@@ -1,53 +1,39 @@
-import android.content.ClipData
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.Nullable
 import androidx.recyclerview.widget.RecyclerView
-import com.amazingtalker.assessment.DateUtility
-import com.amazingtalker.assessment.ItemsViewModel
 import com.amazingtalker.assessment.R
 import com.amazingtalker.assessment.data.Timeline
-import java.text.SimpleDateFormat
-import java.util.*
 
-class CoursesAdapter(private val mList: List<Timeline>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class CoursesAdapter(private val timeLines: List<Timeline>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.card_view_design, parent, false)
+                    .inflate(R.layout.card_view_timeline, parent, false)
         return DesignViewHolder(view)
     }
 
     // binds the list items to a view
     override fun onBindViewHolder(@Nullable holder:  RecyclerView.ViewHolder, position: Int) {
         if (holder is DesignViewHolder) {
-            val ItemsViewModel = mList[position]
+            val timeline = timeLines[position]
 
-            // sets the image to the imageview from our itemHolder class
-            //holder.imageView.setImageResource(ItemsViewModel.image)
-
-            // sets the text to the textview from our itemHolder class
-            holder.textView.text = ItemsViewModel.start + ": " + ItemsViewModel.available
-            ItemsViewModel.start?.let { Log.i("dogtim", it) }
+            holder.textView.text = timeline.start + ": " + timeline.available
+            timeline.start?.let { Log.i("dogtim", it) }
         }
 
     }
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
-        return mList.size
+        return timeLines.size
     }
 
     class DesignViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.imageview)
         val textView: TextView = itemView.findViewById(R.id.textView)
-    }
-    class WeekSelectorViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val textView: TextView = itemView.findViewById(R.id.weekTitle)
     }
 }
